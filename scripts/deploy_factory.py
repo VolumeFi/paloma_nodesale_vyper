3c6115a0053c8599f8072e0a06953b733e5687b9
fullstack412 <jaclindev99@gmail.com>
2025-06-19 09:29:18 -0400

    wip: deploy script

--- scripts/deploy_factory.py
+++ scripts/deploy_factory.py
@@ -6,7 +6,7 @@ def main():
         acct = accounts.load("Deployer")
         blueprint = "0xffD622Ec376f367f2D7CDCA02964E7eF1Cc77465"
         compass = "0x3c1864a873879139C1BD87c7D95c4e475A91d19C"
-        nodesale = ""
+        nodesale = "0xe649F2A20e6563aE3DD60a1e7A5e899C4ef0aeE9"
 
         factory = project.Factory.deploy(blueprint, compass, nodesale, sender=acct)
         print(factory)
